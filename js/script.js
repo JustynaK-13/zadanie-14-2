9589148fb2221786ebebd76ab22a33a6eb289be5
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-07 12:36:21 +0200

    Add new objects with films

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDOM.render(element, document.getElementById('app'));
+var movies = [
+  {
+  	id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  },
+  {
+  	id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny'
+  },
+  {
+	id: 3,
+	title: 'Shreck',
+	desc: 'Film o zielonym ogrze'
+  },
+  {
+  	id: 4,
+  	title: 'Madagaskar',
+  	desc: 'Film o zwierzętach w zoo'
+  }
+];
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc)
+    );
+});
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements)
+  );
+  ReactDOM.render(element, document.getElementById('app'));
